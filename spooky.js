2aa67c6ee6fa4b491875eee754e742580f958335
Dany Cervantes <danycervantesq@gmail.com>
2022-08-29 11:36:29 -0400

    Update spooky.js

--- spooky.js
+++ spooky.js
@@ -2,22 +2,24 @@ function touchOn() {
     document.getElementById("boo").className = "blur:hover";
 }
 
-function touchOff() {
-    document.getElementById("boo").className = "blur";
-}
-
 function start() {
     document.getElementById("boo").addEventListener("touchstart", touchOn());
+}
+
+/*
+function touchOff() {
+    document.getElementById("boo").className = "blur";
 }
 
 function end() {
     document.getElementById("boo").addEventListener("touchend", touchOff());
 }
-
+*/
 
 window.onload = function() {
 
     start();
-    end();
+    
+    //end();
    
 }
